a0224de158f7e19b45288178a5c57a2577c85050
Beirut Bites <beirut.bites.official@gmail.com>
2026-09-05 23:15:00 +0300

    Load burger and review workflow styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 import "./theme.css";
 import "./management.css";
+import "./workflow.css";
 import { PwaRegister } from "@/components/pwa-register";
 
 export const metadata = { title: "24i Production", description: "Agency operating system", manifest: "/manifest.webmanifest" };
